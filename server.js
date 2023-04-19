a6d31c8414625ce8d45bce623a2561bb2016a1c6
Kaif Ahsan <kahsan@atlassian.com>
2023-04-19 22:58:05 +1000

    buildign a basic hello work server

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require("express")
 // The method has been destructured
 const expressGraphQL = require('express-graphql').graphqlHTTP
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
 const app = express()
 
+// Defining a dummy schema
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        // Name cannot have space in between
+        name: 'HelloWorld',
+        // What fields will our query return
+        fields: () => ({
+            message: { 
+                type: GraphQLString,
+                // We need to know how to get the output
+                resolve: () => 'Hello World'
+            }
+        })
+    })
+})
 app.use('/graphql', expressGraphQL({
-    graphiql: true
+    graphiql: true,
+    schema: schema
 }))
 app.listen(5002, () => console.log("Server Running"))
