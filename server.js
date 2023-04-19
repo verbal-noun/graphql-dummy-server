1740afad67b47b1722860593f1fcf61c61670cc9
Kaif Ahsan <kahsan@atlassian.com>
2023-04-19 23:22:53 +1000

    Defining new root query type

--- server.js
+++ server.js
@@ -4,25 +4,59 @@ const expressGraphQL = require('express-graphql').graphqlHTTP
 const {
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
 } = require('graphql')
 const app = express()
 
-// Defining a dummy schema
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        // Name cannot have space in between
-        name: 'HelloWorld',
-        // What fields will our query return
-        fields: () => ({
-            message: { 
-                type: GraphQLString,
-                // We need to know how to get the output
-                resolve: () => 'Hello World'
-            }
-        })
+// Place holder data 
+const authors = [
+	{ id: 1, name: 'J. K. Rowling' },
+	{ id: 2, name: 'J. R. R. Tolkien' },
+	{ id: 3, name: 'Brent Weeks' }
+]
+
+const books = [
+	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+	{ id: 5, name: 'The Two Towers', authorId: 2 },
+	{ id: 6, name: 'The Return of the King', authorId: 2 },
+	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
+	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
+]
+// End of place holder data 
+
+// Creating custom objects 
+const BookType = new GraphQLObjectType({
+    name: "Books",
+    description: "This represnts a bok written by an author.",
+    fields: () => ({
+        id: { type:  GraphQLNonNull(GraphQLInt)},
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt)}
     })
 })
+// Creating a root query
+ const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: "Root query",
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of all books',
+            resolve: () => books
+        }
+    })
+ })
+
+ const schema = new GraphQLSchema({
+    query: RootQueryType
+ })
+
 app.use('/graphql', expressGraphQL({
     graphiql: true,
     schema: schema
